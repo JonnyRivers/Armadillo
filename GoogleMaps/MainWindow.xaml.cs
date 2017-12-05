b3df3d837467fa83a82270a0b89c8d9bfb3e91a8
JonnyRivers <jonny.w.rivers@gmail.com>
2017-12-04 20:52:57 -0800

    Added simple drag scrolling

--- GoogleMaps/MainWindow.xaml.cs
+++ GoogleMaps/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.Logging;
+using System.Windows;
 
 namespace DoogleMaps
 {
@@ -7,13 +8,17 @@ namespace DoogleMaps
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ILogger<MainWindow> _logger;
         private IMainWindowViewModel _viewModel;
+        private Point _mouseDownPosition;
 
-        public MainWindow(IMainWindowViewModel viewModel)
+        public MainWindow(ILogger<MainWindow> logger, IMainWindowViewModel viewModel)
         {
             InitializeComponent();
 
+            _logger = logger;
             _viewModel = viewModel;
+
             DataContext = viewModel;
         }
 
@@ -27,6 +32,33 @@ namespace DoogleMaps
             {
                 --_viewModel.Zoom;
             }
+        }
+
+        private void Image_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Point position = e.GetPosition(null);
+            _logger.LogInformation($"Image_MouseUp @ {position.X},{position.Y}");
+
+            Vector movement = _mouseDownPosition - position;
+            _logger.LogInformation($"Image_MouseUp movement - {movement.X},{movement.Y}");
+
+            double numTiles = System.Math.Pow(2, _viewModel.Zoom);
+            double numPixels = numTiles * 256;
+
+            double pixelsPerLatitude = numPixels / 180;
+            double pixelsPerLongitude = numPixels / 360;
+
+            double latitudeDelta = movement.Y / pixelsPerLatitude;
+            double longitudeDelta = movement.X / pixelsPerLongitude;
+
+            _viewModel.Latitude -= latitudeDelta;
+            _viewModel.Longitude += longitudeDelta;
+        }
+
+        private void Image_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            _mouseDownPosition = e.GetPosition(null);
+            _logger.LogInformation($"Image_MouseDown @ {_mouseDownPosition.X},{_mouseDownPosition.Y}");
         }
     }
 }
